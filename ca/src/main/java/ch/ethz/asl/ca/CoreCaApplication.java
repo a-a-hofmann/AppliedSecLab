c199c8340db8c621608fe5c4c332df0fbb52b00f
Ard Kastrati <ard_kitara@hotmail.com>
2017-11-01 15:20:36 +0100

    Test repo

--- ca/src/main/java/ch/ethz/asl/ca/CoreCaApplication.java
+++ ca/src/main/java/ch/ethz/asl/ca/CoreCaApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @SpringBootApplication
 @RestController
 //@EnableResourceServer // TODO: Add back in to use oauth2
