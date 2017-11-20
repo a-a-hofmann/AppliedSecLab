1df0bf282ab914b46c5190105996bf2a1cd89011
Ard Kastrati <ard_kitara@hotmail.com>
2017-11-20 20:29:52 +0100

    Removed the password test for the p12 files

--- ca/src/main/java/ch/ethz/asl/ca/service/command/OpenSSL.java
+++ ca/src/main/java/ch/ethz/asl/ca/service/command/OpenSSL.java
@@ -52,7 +52,7 @@ public class OpenSSL implements CertificateManager {
     private final String SIGN_CERTIFICATE = "openssl ca -name CA_%s -batch -in %s.csr -config " + ABSOLUTE_DIR + "openssl.cnf -passin pass:admin";
     private final String REVOKE_CERTIFICATE = "openssl ca -revoke %s -config " + ABSOLUTE_DIR + "openssl.cnf -passin pass:admin";
     private final String CREATE_CRL = "openssl ca -gencrl -out " + ABSOLUTE_DIR + "CA/crl/crl.pem  -config " + ABSOLUTE_DIR + "openssl.cnf -passin pass:admin";
-    private final String CREATE_P12 = "openssl pkcs12 -export -out %s -inkey %s -in %s -passout pass:test";
+    private final String CREATE_P12 = "openssl pkcs12 -export -out %s -inkey %s -in %s -passout pass:";
 
     //private final UserCertificateService userCertificateService;
 
