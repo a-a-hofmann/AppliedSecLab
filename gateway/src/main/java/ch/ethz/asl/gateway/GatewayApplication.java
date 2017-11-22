6ff0a4f6faea631db145f4769ac035f9671a4033
Alexander Hofmann <alexhofmann@gmail.com>
2017-11-22 21:39:27 +0100

    Add http to https redirect.

--- gateway/src/main/java/ch/ethz/asl/gateway/GatewayApplication.java
+++ gateway/src/main/java/ch/ethz/asl/gateway/GatewayApplication.java
@@ -1,10 +1,17 @@
 package ch.ethz.asl.gateway;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -23,12 +30,43 @@ public class GatewayApplication extends ResourceServerConfigurerAdapter {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
+    @Bean
+    public EmbeddedServletContainerFactory tomcatEmbeddedServletContainerFactory() {
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+
+        tomcat.addAdditionalTomcatConnectors(createConnector());
+        return tomcat;
+    }
+
+    private Connector createConnector() {
+        final String protocol = "org.apache.coyote.http11.Http11NioProtocol";
+        final Connector connector = new Connector(protocol);
+        connector.setScheme("http");
+        connector.setSecure(false);
+        connector.setPort(8080);
+        connector.setRedirectPort(8443);
+
+        return connector;
+    }
+
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http
+                .requiresChannel().anyRequest().requiresSecure()
+                .and()
                 .logout().and()
                 .authorizeRequests()
-                .antMatchers("/login").permitAll()
+                .antMatchers("/login", "/fonts/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .csrf()
